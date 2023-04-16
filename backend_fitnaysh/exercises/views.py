8cc8c963065996c68d68d32920e2710764e40c6e
Abhijit-Kadalli <abhijitkadalli@nitgoa.ac.in>
2023-04-16 09:01:31 +0530

    backend ready with 3 exer squat,deadlift,bicepcurl

--- backend_fitnaysh/exercises/views.py
+++ backend_fitnaysh/exercises/views.py
@@ -13,3 +13,23 @@ def curl_video_feed(request):
 def curl_video(request):
     context = {'counter': fn.counter, 'stage': fn.stage}
     return render(request, 'bicep_curl_video.html',context)
+
+def squat_video_feed(request):
+    return StreamingHttpResponse(fn.squat_counter(),
+                                 content_type='multipart/x-mixed-replace; boundary=frame')
+
+def squat_video(request):
+    context = {'counter': fn.counter, 
+               'stage': fn.stage,
+               'back_form': fn.back_form,
+               'heelisGrounded': fn.heelIsGrounded,
+               'notlowSquat': fn.not_low_squat,}
+    return render(request, 'squat_video.html',context)
+
+def deadlift_video_feed(request):
+    return StreamingHttpResponse(fn.deadlift_counter(),
+                                 content_type='multipart/x-mixed-replace; boundary=frame')
+
+def deadlift_video(request):
+    context = {'counter': fn.counter, 'stage': fn.stage, 'back_form': fn.back_form}
+    return render(request, 'deadlift_video.html',context)
